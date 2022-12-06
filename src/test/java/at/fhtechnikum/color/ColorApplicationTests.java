bf663541a3730171d843cb4601a014a1f045637b
Erwin Li <ic19b005@technikum-wien.at>
2022-12-06 19:27:53 +0100

    #2 get complimentary color code

--- src/test/java/at/fhtechnikum/color/ColorApplicationTests.java
+++ src/test/java/at/fhtechnikum/color/ColorApplicationTests.java
@@ -1,13 +1,16 @@
 package at.fhtechnikum.color;
 
+import at.fhtechnikum.color.Controller.ColorController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ColorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getCompColorTest() {
+		//assertEquals("The complementary color of RED is GREEN", ColorController.("red"));
 	}
 
 }
